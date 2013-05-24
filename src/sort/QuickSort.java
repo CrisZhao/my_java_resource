39324c50b853f305a8f474ec0e6e41d522f944ea
criszhao <zhaoqingce@gmail.com>
2013-05-24 18:14:15 +0800

    effective quick sort

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,8 +1,5 @@
 package sort;
 
-import java.util.Arrays;
-import java.util.Random;
-
 /**
  * An implementation of insert sort algorithm.
  * 
@@ -16,33 +13,31 @@ public class QuickSort {
 
 	}
 
-	private int partition(int[] array, int low, int high) {
-		int index = low;
-		int value = getPartitionValue(array, low, high);
-		for (int i = low; i < high; i++) {
-			if (array[i] <= value) {
-				SwapUtils.swap(array, index++, i);
-				System.out.println(Arrays.toString(array));
-			}
-		}
-		SwapUtils.swap(array, index, high);
-		return index;
-	}
-
-	private int getPartitionValue(int[] array, int low, int high) {
-		int r = new Random().nextInt(high - low + 1);
-		// put the value in the end of the array
-		SwapUtils.swap(array, low + r, high);
-		return array[high];
-	}
-
 	private void quickSort(int[] array, int low, int high) {
 		if (high <= low) {
 			return;
 		}
-		int index = partition(array, low, high);
-		quickSort(array, low, index - 1);
-		quickSort(array, index + 1, high);
+		int lo = low;
+		int hi = high;
+		int mid = array[(low + high) / 2];
+
+		while (true) {
+			while ((lo < high) && (array[lo] < mid))
+				lo++;
+			while ((hi > low) && (array[hi] > mid))
+				hi--;
+			if (lo > hi)
+				break;
+			if (lo < hi) {
+				SwapUtils.swap(array, lo, hi);
+			}
+			lo++;
+			hi--;
+		}
+		if (low < hi)
+			quickSort(array, low, hi);
+		if (lo < high)
+			quickSort(array, lo, high);
 	}
 
 }
