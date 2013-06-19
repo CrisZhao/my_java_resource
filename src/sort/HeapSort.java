a3c124c206127ece4940d9e7e36729872685d67f
criszhao <zhaoqingce@gmail.com>
2013-06-19 11:54:57 +0800

    add heapsort

--- src/sort/HeapSort.java
+++ src/sort/HeapSort.java
@@ -1,31 +1,57 @@
 package sort;
 
+import org.junit.Test;
+
 public class HeapSort {
-	private void adjustOnce(int[] array, int index) {
-		int length = array.length;
-		int left = 2*index +1;
-		int right = 2*index +2;
-		int smallestIndex = index;
-			if(left<length && array[left] < array[smallestIndex]){
-				smallestIndex = left;
-			}
-			if(right<length && array[right] < array[smallestIndex]){
-				smallestIndex = right;
-			}
-			if(smallestIndex!=index) {
-				SwapUtils.swap(array, smallestIndex, index);
-			}
-		
-		
+	private void heapify(int[] array, int length, int index) {
+		int left = left(index);
+		int right = right(index);
+		int maxIndex = index;
+		if (left < length) {
+			maxIndex = array[left] > array[maxIndex]?left:maxIndex;
+		}
+		if (right < length) {
+			maxIndex = array[right] > array[maxIndex]?right:maxIndex;
+		}
+		if (maxIndex != index) {
+			SwapUtils.swap(array, maxIndex, index);
+			heapify(array, length, maxIndex);
+		}
+
 	}
-	public void sort(int[] array) {
+
+	private void buildHeap(int[] array, int length) {
 		int index;
-			index = (array.length-1)/2;
-			for (int j = index; index>=0;index--) {
-				adjustOnce(array, j);
-			}
-			
+		index = (length - 1) / 2;
+		for (int j = index; j >= 0; j--) {
+			heapify(array,length, j);
+		}
+
 	}
 	
+	public void sort(int[] array) {
+		buildHeap(array, array.length);
+		int unSortedLength = array.length;
+		while(unSortedLength>1) {
+			SwapUtils.swap(array, 0, --unSortedLength);
+			heapify(array, unSortedLength, 0);
+		}
+		
+	}
+	@Test
+	public void tst() {
+		int[] array = new int[] {2,4,5,6,8,9,3,2,1};
+		sort(array);
+		System.out.println(array);
+	}
+
+
+	private int left(int index) {
+		return 2 * index+1;
+	}
+
+	private int right(int index) {
+		return 2 * index + 2;
+	}
 
 }
