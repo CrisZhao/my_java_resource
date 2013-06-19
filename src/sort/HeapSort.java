70c37b120b0525cfac42ca33718033e46eec9c25
criszhao <zhaoqingce@gmail.com>
2013-06-19 16:08:24 +0800

    modify heapsort

--- src/sort/HeapSort.java
+++ src/sort/HeapSort.java
@@ -8,10 +8,10 @@ public class HeapSort {
 		int right = right(index);
 		int maxIndex = index;
 		if (left < length) {
-			maxIndex = array[left] > array[maxIndex]?left:maxIndex;
+			maxIndex = array[left] > array[maxIndex] ? left : maxIndex;
 		}
 		if (right < length) {
-			maxIndex = array[right] > array[maxIndex]?right:maxIndex;
+			maxIndex = array[right] > array[maxIndex] ? right : maxIndex;
 		}
 		if (maxIndex != index) {
 			SwapUtils.swap(array, maxIndex, index);
@@ -24,30 +24,30 @@ public class HeapSort {
 		int index;
 		index = (length - 1) / 2;
 		for (int j = index; j >= 0; j--) {
-			heapify(array,length, j);
+			heapify(array, length, j);
 		}
 
 	}
-	
+
 	public void sort(int[] array) {
 		buildHeap(array, array.length);
-		int unSortedLength = array.length;
-		while(unSortedLength>1) {
-			SwapUtils.swap(array, 0, --unSortedLength);
-			heapify(array, unSortedLength, 0);
+		int heapNum = array.length;
+		while (heapNum > 1) {
+			SwapUtils.swap(array, 0, --heapNum);
+			heapify(array, heapNum, 0);
 		}
-		
+
 	}
+
 	@Test
 	public void tst() {
-		int[] array = new int[] {2,4,5,6,8,9,3,2,1};
+		int[] array = new int[] { 2, 4, 5, 6, 8, 9, 3, 2, 1 };
 		sort(array);
 		System.out.println(array);
 	}
 
-
 	private int left(int index) {
-		return 2 * index+1;
+		return 2 * index + 1;
 	}
 
 	private int right(int index) {
